81df6e92b0dc0b55b6281b8bc88619a3cadb5a8b
jiha-ftw <75131212+jiha-ftw@users.noreply.github.com>
2023-01-07 15:53:11 +0100

    Change routing to break integration tests (#1)
    * Change routing to break integration tests
    
    * Fix
    
    * Add Cypress record key
    
    * run-tests.yml Add build step with build arg
    
    * Add --no-recreate
    
    * echo test
    
    * run-tests.yml Env variable
    
    * run-tests.yml Env variable
    
    * run-tests.yml Env variable
    
    * run-tests.yml Env variable
    
    * run-tests.yml Env variable
    
    * Update Az pipeline
    
    * Update pipeline.yml for Azure Pipelines
    
    * Update pipeline.yml for Azure Pipelines
    
    * Update pipeline.yml for Azure Pipelines
    
    * Update pipeline.yml for Azure Pipelines
    
    * Update pipeline.yml for Azure Pipelines
    
    * Update pipeline.yml for Azure Pipelines

--- cypress.io/cypress.config.js
+++ cypress.io/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    video: false,
+    projectId: "q8ktxg",
     baseUrl: 'https://localhost:7036/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
